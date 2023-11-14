2c7d73621bc5c82efaba31912cde31919ce697c8
AndrésLabat <andreslabat89@gmail.com>
2023-11-14 16:31:41 +0100

    feat: added route * in body.jsx to Home

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../Home/Home';
 import { Login } from '../Login/Login';
 import { Register } from '../Register/Register';
@@ -13,22 +13,23 @@ import { GetAllAppointments } from '../GetAllAppointments/GetAllAppointments';
 import { Portfolio } from '../Portfolio/Portfolio';
 
 export const Body = () => {
-     return (
-         <>
+    return (
+        <>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/portfolio" element={<Portfolio />}/>
-                <Route path="/workers" element={<GetWorkers />}/>
-                <Route path="/register" element={<Register />}/>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/profile" element={<Profile />}/>
-                <Route path="/updateProfile" element={<UpdateProfile />}/>
-                <Route path="/appointments" element={<Appointments />}/>
-                <Route path="/createAppointment" element={<CreateAppointment />}/>
-                <Route path="/updateAppointment" element={<UpdateAppointment />}/>
-                <Route path="/getAllUsers" element={<GetAllUsers />}/>
-                <Route path="/getAllAppointments" element={<GetAllAppointments />}/>
+                <Route path="*" element={<Navigate to="/" />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/workers" element={<GetWorkers />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/updateProfile" element={<UpdateProfile />} />
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/createAppointment" element={<CreateAppointment />} />
+                <Route path="/updateAppointment" element={<UpdateAppointment />} />
+                <Route path="/getAllUsers" element={<GetAllUsers />} />
+                <Route path="/getAllAppointments" element={<GetAllAppointments />} />
             </Routes>
-         </>
-     )
+        </>
+    )
 }
